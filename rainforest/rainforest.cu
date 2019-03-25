91d11f1f523ea6d0763d840be404e66d52bbb96f
djm34 <djm34@aliceadsl.fr>
2019-03-25 16:33:00 +0200

    initial commit

---
+++ rainforest/rainforest.cu
@@ -0,0 +1,121 @@
+/**
+ * Rainforest algorithm
+ * djm34 2019
+ */
+
+#include <string.h>
+#include <stdint.h>
+
+extern "C" {
+#include "rainforest.h"
+}
+
+#include <cuda_helper.h>
+#include <miner.h>
+
+#define A 64
+#define debug_cpu 0
+
+/* ############################################################################################################################### */
+
+
+extern void rainforest_init(int thr_id, uint32_t threads);
+extern void rainforest_setBlockTarget(int thr_id, const void* pDataIn, const void *pTargetIn, const void * zElement);
+extern uint32_t rainforest_cpu_hash(int thr_id, uint32_t threads, uint32_t startNounce);
+
+static bool init[MAX_GPUS] = { 0 };
+
+
+
+extern "C" int scanhash_rf256(int thr_id, struct work *work, uint32_t max_nonce, unsigned long *hashes_done)
+{
+	uint32_t _ALIGN(64) hash[8];
+	uint32_t _ALIGN(64) endiandata[20];
+	uint32_t *pdata = work->data;
+	uint32_t *ptarget = work->target;
+	const uint32_t Htarg = ptarget[7];
+	const uint32_t first_nonce = pdata[19];
+	uint32_t nonce = first_nonce;
+
+//	rf256_ctx_t ctx, ctx_common;
+
+	const int dev_id = device_map[thr_id];
+	int intensity = (device_sm[dev_id] > 500 && !is_windows()) ? 22 : 20;
+	if (device_sm[dev_id] >= 600) intensity = 23;
+	if (device_sm[dev_id] < 350) intensity = 18;
+
+	uint32_t throughput = cuda_default_throughput(thr_id, 1U << intensity);
+	if (init[thr_id]) throughput = min(throughput, max_nonce - first_nonce);
+
+
+	if (opt_benchmark) {
+		ptarget[7] = 0x0cff;
+	}
+
+
+	if (!init[thr_id]) {
+		cudaSetDevice(dev_id);
+//		if (opt_cudaschedule == -1 && gpu_threads == 1) {
+			cudaDeviceReset();
+			// reduce cpu usage
+			cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
+			cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
+			CUDA_LOG_ERROR();
+//		}
+		gpulog(LOG_INFO, thr_id, "Intensity set to %g, %u cuda threads", throughput2intensity(throughput), 
+			throughput);
+
+		rainforest_init(thr_id,throughput);
+		CUDA_LOG_ERROR();
+		init[thr_id] = true;
+	}
+
+
+	for (int k = 0; k < 19; k++) {
+		be32enc(&endiandata[k], pdata[k]);
+	}
+
+	// pre-compute the hash state based on the constant part of the header
+
+	unsigned char ThePreData[4128*4];
+	rainforest_precompute(endiandata,ThePreData);
+
+//	rainforest_precompute(pdata, ThePreData);
+	rainforest_setBlockTarget(thr_id, endiandata,ptarget,ThePreData);
+	do {
+		
+		work->nonces[0] = rainforest_cpu_hash(thr_id, throughput, pdata[19]);
+
+		if (work->nonces[0] != UINT32_MAX)
+		{
+		be32enc(&endiandata[19], work->nonces[0]);
+		
+		rf256_hash(hash, endiandata, 80);
+
+		if (((uint64_t*)hash)[3] <= ((uint64_t*)ptarget)[3]) {
+//			if (hash[7] <= Htarg && fulltest(hash, ptarget)) {
+			int res = 1;
+			work_set_target_ratio(work, hash);
+			pdata[19] = work->nonces[0];
+			*hashes_done = pdata[19] - first_nonce;
+			return res;
+		}
+		else {
+			gpulog(LOG_WARNING, thr_id, "result for %08x does not validate on CPU!", work->nonces[0]);
+		}
+
+
+		}
+	
+
+//////////////////////////////////////////////////////
+		if ((uint64_t)throughput + pdata[19] >= max_nonce) {
+			pdata[19] = max_nonce;
+			break;
+		}
+		pdata[19] += throughput;
+
+	} while (nonce < max_nonce && !work_restart[thr_id].restart);
+	*hashes_done = pdata[19] - first_nonce;
+	return 0;
+}
---
+++ rainforest/rainforest.h
@@ -0,0 +1,95 @@
+// RainForest hash algorithm
+// Author: Bill Schneider
+// Date: Feb 13th, 2018
+//
+// RainForest uses native integer operations which are extremely fast on
+// modern 64-bit processors, significantly slower on 32-bit processors such
+// as GPUs, and extremely slow if at all implementable on FPGAs and ASICs.
+// It makes an intensive use of the L1 cache to maintain a heavy intermediary
+// state favoring modern CPUs compared to GPUs (small L1 cache shared by many
+// shaders) or FPGAs (very hard to implement the required low-latency cache)
+// when scanning ranges for nonces. The purpose is to create a fair balance
+// between all mining equipments, from mobile phones to extreme performance
+// GPUs and to rule out farming factories relying on ASICs and FPGAs. The
+// CRC32 instruction is used a lot as it is extremely fast on low-power ARM
+// chips and allows such devices to rival high-end PCs mining performance.
+//
+// Tests on various devices have shown the following performance :
+// +--------------------------------------------------------------------------+
+// | CPU/GPU       Clock Threads Full hash  Nonce scan  Watts   Cost          |
+// |               (MHz)         (80 bytes) (4 bytes)   total                 |
+// | Core i7-6700k  4000      8   390 kH/s  1642 kH/s     200  ~$350+PC       |
+// | Radeon RX560   1300   1024  1100 kH/s  1650 kH/s     300  ~$180+PC       |
+// | RK3368 (8*A53) 1416      8   534 kH/s  1582 kH/s       6   $60 (Geekbox) |
+// +--------------------------------------------------------------------------+
+//
+// Build instructions on Ubuntu 16.04 :
+//   - on x86:   use gcc -march=native or -maes to enable AES-NI
+//   - on ARMv8: use gcc -march=native or -march=armv8-a+crypto+crc to enable
+//               CRC32 and AES extensions.
+//
+// Note: always use the same options to build all files!
+
+#include "miner.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+// this seems necessary only for gcc, otherwise hash is bogus
+#ifdef _MSC_VER
+typedef uint8_t  rf_u8;
+typedef uint16_t rf_u16;
+typedef uint32_t rf_u32;
+typedef uint64_t rf_u64;
+#else
+typedef __attribute__((may_alias)) uint8_t  rf_u8;
+typedef __attribute__((may_alias)) uint16_t rf_u16;
+typedef __attribute__((may_alias)) uint32_t rf_u32;
+typedef __attribute__((may_alias)) uint64_t rf_u64;
+#endif
+
+// 2048 entries for the rambox => 16kB
+#define RAMBOX_SIZE 2048
+#define RAMBOX_LOOPS 4
+
+
+typedef union {
+	rf_u8  b[32];
+	rf_u16 w[16];
+	rf_u32 d[8];
+	rf_u64 q[4];
+} hash256_t;
+/*
+typedef struct _ALIGN(16) rf_ctx {
+	uint64_t rambox[RAMBOX_SIZE];
+	hash256_t hash;
+	uint32_t crc;
+	uint32_t word;  // LE pending message
+	uint32_t len;   // total message length
+} rf256_ctx_t;
+*/
+
+typedef struct _ALIGN(128) rf_ctx {
+	uint32_t word; // LE pending message
+	uint32_t len; // total message length
+	uint32_t crc;
+	//	uint32_t changes; // must remain lower than RAMBOX_HIST
+	_ALIGN(32) hash256_t hash;
+	//	uint16_t hist[RAMBOX_HIST];
+	_ALIGN(64) uint64_t rambox[RAMBOX_SIZE];
+} rf256_ctx_t;
+
+// initialize the hash state
+// void rf256_init(rf256_ctx_t *ctx);
+
+
+// update the hash context _ctx_ with _len_ bytes from message _msg_
+// void rf256_update(rf256_ctx_t *ctx, const void *msg, size_t len);
+
+// finalize the hash and copy the result into _out_ if not null (256 bits)
+// void rf256_final(void *out, rf256_ctx_t *ctx);
+
+// hash _len_ bytes from _in_ into _out_
+void rf256_hash(void *out, const void *in, size_t len);
+void rainforest_precompute(const void *in, void *out);
